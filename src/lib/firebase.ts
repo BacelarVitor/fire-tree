32e482dc050d051fa6e4f42f9345b0bbafa9ff49
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-13 14:57:28 -0300

    improviments on store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth'
 import { getStorage} from 'firebase/storage'
 import { writable } from "svelte/store";
 // Your web app's Firebase configuration
@@ -21,10 +21,19 @@ export const auth = getAuth();
 export const storage = getStorage();
 
 function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return { subscribe }
+  }
   const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
-    onAuthStateChanged(auth, (user) =>{
+    unsubscribe = onAuthStateChanged(auth, (user) =>{
       set(user)
     });
+
+    return () => unsubscribe();
   });
 
   return {
