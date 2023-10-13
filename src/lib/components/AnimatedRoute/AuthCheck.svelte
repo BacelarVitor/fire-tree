4911455556256ee46983776e581615e3f5fb9622
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-13 15:08:19 -0300

    implement check if user is logged

---
+++ src/lib/components/AnimatedRoute/AuthCheck.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { auth, user } from '$lib/firebase'
+    import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+        console.log('fon!', user)
+    }
+
+</script>
+
+{#if $user}
+    <slot/>
+{:else}
+    <p class="text-error my-10">
+        You must be signed in to view this page.
+        <a href="/logn" class="btn btn-primary">Sign in</a>
+    </p>
+{/if}
